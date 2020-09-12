1cd9f6afcc3f2026fc3e14343bfc2bb8b42e4a0e
Benny <bennyhinnotieno@gmail.com>
2020-09-12 23:09:31 +0300

    Updates

---
+++ precache-manifest.de31b4400336d39e4a1dd8fb432db8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f67d3c8f7c68459f7cb8d8b4ea8ef72",
+    "url": "/landing_page_design/index.html"
+  },
+  {
+    "revision": "90791b11aba8a5f291db",
+    "url": "/landing_page_design/static/css/main.39acd7f9.chunk.css"
+  },
+  {
+    "revision": "dcc729181f3128df61c7",
+    "url": "/landing_page_design/static/js/2.27cfdf7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing_page_design/static/js/2.27cfdf7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90791b11aba8a5f291db",
+    "url": "/landing_page_design/static/js/main.0793e0d9.chunk.js"
+  },
+  {
+    "revision": "9e2befd4bf8c06ff4dd9",
+    "url": "/landing_page_design/static/js/runtime-main.de0d43bd.js"
+  }
+]);
